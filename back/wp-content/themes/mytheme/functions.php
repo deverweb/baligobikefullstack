b372769e3459681faf7b26d3325b111cf591bbff
deverweb <andreydever@gmail.com>
2023-04-12 20:50:07 +0400

    sds

--- back/wp-content/themes/mytheme/functions.php
+++ back/wp-content/themes/mytheme/functions.php
@@ -3,7 +3,7 @@
 function add_cors_http_header(){
 	header("Access-Control-Allow-Origin: *");
 }
-add_action('init', 'baligo_init')
+add_action('init', 'baligo_init');
 
 function baligo_init() {
 	register_post_type('bike', [
