ccf65234730339df1c94eff35a65fd9a6d8e5bcc
deverweb <andreydever@gmail.com>
2023-04-23 10:59:02 +0400

    sdsd

--- back/wp-content/themes/mytheme/functions.php
+++ back/wp-content/themes/mytheme/functions.php
@@ -20,70 +20,28 @@ add_action('rest_api_init', function () {
 });
 
 
-function myplugin_sanitize_file_name( $file_name ) {
-	$sanitized_name = sanitize_file_name( $file_name );
-	$extension = pathinfo( $sanitized_name, PATHINFO_EXTENSION );
-	$unique_name = md5( uniqid( rand(), true ) ) . '.' . $extension;
-	return $unique_name;
-}
 
-function markers_save_file( $request ) {
-	$uploaded_file = $request->get_file_params()['file'];
 
-	// проверяем формат файла
-	if ( ! in_array( $uploaded_file['type'], array( 'image/png', 'image/jpeg', 'image/jpg' ) ) ) {
-			return new WP_Error( 'invalid_file_type', 'Формат файла не поддерживается. Поддерживаются только PNG, JPEG и JPG', array( 'status' => 400 ) );
-	}
-
-	// загружаем файл на сервер
-	$upload = wp_upload_bits( $uploaded_file['name'], null, file_get_contents( $uploaded_file['tmp_name'] ) );
-
-	// проверяем на ошибки при загрузке файла
-	if ( isset( $upload['error'] ) && $upload['error'] ) {
-			return new WP_Error( 'upload_error', $upload['error'], array( 'status' => 400 ) );
-	}
-
-	// добавляем загруженный файл в медиабиблиотеку
-	$file_path = $upload['file'];
-	$file_name = basename( $file_path );
-	$attachment = array(
-			'post_mime_type' => $upload['type'],
-			'post_title' => preg_replace( '/\.[^.]+$/', '', $file_name ),
-			'post_content' => '',
-			'post_status' => 'inherit'
-	);
-	$attachment_id = wp_insert_attachment( $attachment, $file_path );
-	require_once( ABSPATH . 'wp-admin/includes/image.php' );
-	$attachment_data = wp_generate_attachment_metadata( $attachment_id, $file_path );
-	wp_update_attachment_metadata( $attachment_id, $attachment_data );
-
-	// получаем URL загруженного файла
-	$image_url = wp_get_attachment_url( $attachment_id );
-
-	// возвращаем ссылку на загруженный файл
-	return array( 'file_url' => $image_url );
-}
-
-// function markers_save_file(WP_REST_Request $request) {
-//   $file = $request->get_file_params()['file'];
-//   if (!$file) {
-//     return new WP_Error('no_file', 'Файл не найден.', ['status' => 400]);
-//   }
-//   $file_type = wp_check_filetype($file['name']);
-//   if (!$file_type || !in_array($file_type['ext'], ['jpg', 'jpeg', 'png'])) {
-//     return new WP_Error('invalid_file_type', 'Пожалуйста, выберите только изображение!', ['status' => 400]);
-//   }
+function markers_save_file(WP_REST_Request $request) {
+  $file = $request->get_file_params()['file'];
+  if (!$file) {
+    return new WP_Error('no_file', 'Файл не найден.', ['status' => 400]);
+  }
+  $file_type = wp_check_filetype($file['name']);
+  if (!$file_type || !in_array($file_type['ext'], ['jpg', 'jpeg', 'png'])) {
+    return new WP_Error('invalid_file_type', 'Пожалуйста, выберите только изображение!', ['status' => 400]);
+  }
  
-//   $upload_dir = wp_upload_dir();
-// 	$file_name = uniqid() . '.' . $file_type['ext'];
-//   $target_dir = $upload_dir['path'] . '/' . $file_name;
-//   $target_url = $upload_dir['url'] . '/' . $file_name;
-//   if (move_uploaded_file($file['tmp_name'], $target_dir)) {
-//     return ['file_url' => $target_url];
-//   } else {
-//     return new WP_Error('upload_failed', 'Не удалось загрузить файл.', ['status' => 500]);
-//   }
-// }
+  $upload_dir = wp_upload_dir();
+	$file_name = uniqid() . '.' . $file_type['ext'];
+  $target_dir = $upload_dir['path'] . '/' . $file_name;
+  $target_url = $upload_dir['url'] . '/' . $file_name;
+  if (move_uploaded_file($file['tmp_name'], $target_dir)) {
+    return ['file_url' => $target_url];
+  } else {
+    return new WP_Error('upload_failed', 'Не удалось загрузить файл.', ['status' => 500]);
+  }
+}
 
 add_action('init', 'baligo_init');
 function baligo_init() {
